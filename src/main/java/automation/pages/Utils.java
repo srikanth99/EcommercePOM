0a8873b43045a79b54dd05c7e99e2b2e6133dcad
srikanthgoudm <msg.9985168472@gmail.com>
2017-10-22 09:40:16 +0100

    added waitForElement Method

--- src/main/java/automation/pages/Utils.java
+++ src/main/java/automation/pages/Utils.java
@@ -3,7 +3,9 @@ package automation.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utils extends BasePage {
 	public static boolean isElementPresent(By element) {
@@ -17,8 +19,12 @@ public class Utils extends BasePage {
 	
 public static void scrollbyY(WebElement elementlocator) {
 	WebElement ele=driver.findElement((By) elementlocator);
-	((JavascriptExecutor)driver).executeScript("window.scrollTo(0,"+ele.getLocation().y+")");
+	((JavascriptExecutor)driver).executeScript("window.scrollTo(0," + ele.getLocation().y + ")");
 	
 }
+	public static void waitForElementVisible(WebElement element, int time) {
+		WebDriverWait wait = new WebDriverWait(driver,time);
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
 
 }

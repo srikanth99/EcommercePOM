417c38275b79c46cea6729534bc94ffd66805cda
module1 <praveenkayyala@gmail.com>
2018-01-19 13:56:28 +0000

    bbc class added in project

---
+++ src/test/java/automation/BbcNews.java
@@ -0,0 +1,24 @@
+package automation;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by Sudhakar on 19/01/2018.
+ */
+public class BbcNews {
+    WebDriver driver;
+    @Test
+    public void bbc(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.bbc.co.uk");//https://www.bbc.co.uk/
+        String title=driver.getTitle();
+        System.out.println("this is page tille "+title);
+        driver.close();
+        System.out.println("closing -------");
+
+    }
+}
